4172a7aa6e9a96a116a560152406c0b72f14997a
osamaashraf6 <osamaashraf668@gmail.com>
2022-10-12 13:41:54 +0200

    Fix the problem at the js file

--- static/js/index.js
+++ static/js/index.js
@@ -3,30 +3,25 @@ const burger = document.querySelector(
   '.header__container__parhead__nav__ul__responsive',
 );
 const iwq = document.querySelector('.iwq');
-const navLinks = document.querySelectorAll('.mobile-nav-links')
-
+const navLinks = document.querySelectorAll('.mobile-nav-links');
+// ==================================================================================
 burger.addEventListener('click', () => {
   if (burger.classList.contains('change')) {
     burger.classList.remove('change');
-    navLinks.forEach((item) => {
-      item.addEventListener('click', () => {
-        burger.classList.remove('change');
-      });
-    });
   } else {
     burger.classList.add('change');
   }
 
   if (iwq.classList.contains('show')) {
     iwq.classList.remove('show');
-    navLinks.forEach((item) => {
-    item.addEventListener('click', () => {
-    iwq.classList.remove('show');
-      });
-    });
   } else {
     iwq.classList.add('show');
   }
-
-  // iwq.style.display = "flex";
+});
+// ====================================================================================
+navLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    iwq.classList.remove('show');
+    burger.classList.remove('change');
+  });
 });
